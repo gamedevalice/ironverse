8dcf258ed0108a690d7b386bb8100f7e966c9fb8
ChaoticGood <colete_nico@yahoo.com>
2023-08-19 21:17:53 +0800

    Implemented example for using depth, voxel_scale and range for loading chunks

--- crates/voxels/src/chunk/chunk_manager.rs
+++ crates/voxels/src/chunk/chunk_manager.rs
@@ -60,18 +60,17 @@ pub struct ChunkManager {
   pub height_scale: f64,
   pub frequency: f64,
 
-  pub lod_dist: Vec<i64>,
-  pub ray_dist: i64,
-  pub voxel_reuse: VoxelReuse,
-  pub config: Configuration,
+  pub voxel_scale: f32,
+  pub range: u8,
 }
 
 impl Default for ChunkManager {
   fn default() -> Self {
     let depth = 4;
-    let loop_count = 3; // indices/axes being used, [x, y, z]
+    // let loop_count = 3; // indices/axes being used, [x, y, z]
+    // let voxel_reuse = VoxelReuse::new(depth, loop_count);
+    
     let noise = OpenSimplex::new().set_seed(1234);
-    let voxel_reuse = VoxelReuse::new(depth, loop_count);
     let offset = 2;
     let chunk_size = 2_i32.pow(depth) as u32;
 
@@ -84,25 +83,33 @@ impl Default for ChunkManager {
       noise: noise,
       height_scale: 16.0,
       frequency: 0.0125,
-
-      lod_dist: vec![1, 2],
-      ray_dist: 5,
-      voxel_reuse: voxel_reuse.clone(),
-
-      // Used for player simulator
-      config: Configuration {
-        depth: depth as u8, 
-        lod: depth as u8, 
-        noise: noise,
-        voxel_reuse: voxel_reuse.clone(),
-        seamless_size: chunk_size - offset,
-        chunk_size: chunk_size,
-      }
+      voxel_scale: 1.0,
+      range: 1,
     }
   }
 }
 
 impl ChunkManager {
+
+  pub fn new(depth: u32, voxel_scale: f32, range: u8) -> Self {
+    let noise = OpenSimplex::new().set_seed(1234);
+    let offset = 2;
+    let chunk_size = 2_i32.pow(depth) as u32;
+
+    ChunkManager {
+      chunks: HashMap::new(),
+      colliders: HashMap::new(),
+      depth: depth,
+      chunk_size: chunk_size,
+      offset: offset,
+      noise: noise,
+      height_scale: 16.0,
+      frequency: 0.0125,
+      voxel_scale: voxel_scale,
+      range: range,
+    }
+  }
+
   /* TODO: Remove later */
   pub fn set_voxel1(&mut self, pos: &[i64; 3], voxel: u8) -> Vec<[i64; 3]> {
     let mut keys = Vec::new();
@@ -402,27 +409,28 @@ impl ChunkManager {
     self.chunks.len()
   }
 
-  // FOR TESTING
-  pub fn get_noise(&self, x: f64, z: f64, middle: i64) -> i64 {
-    let fx = (x as i64 - middle) as f64 * self.frequency;
-    let fz = (z as i64 - middle) as f64 * self.frequency;
-    let noise = self.noise.get([fx, fz]);
-    let elevation = (noise * self.height_scale) as i64;
-    elevation
+  
+
+  pub fn get_adj_chunks(&mut self, key: [i64; 3]) -> Vec<Chunk> {
+    let mut chunks = Vec::new();
+
+    let keys = adjacent_keys(&key, self.range as i64, true);
+    for key in keys.iter() {
+      let res = self.chunks.get(key);
+      if res.is_some() {
+        chunks.push(res.unwrap().clone());
+      }
+
+      if res.is_none() {
+        let c = self.new_chunk3(key, self.depth as u8);
+        chunks.push(c.clone());
+        self.chunks.insert(*key, c);
+      }
+    }
+
+    chunks
   }
 }
-
-#[derive(Clone)]
-pub struct Configuration {
-  pub depth: u8,
-  pub lod: u8,
-  pub noise: OpenSimplex,
-  pub voxel_reuse: VoxelReuse,
-  pub seamless_size: u32,
-  pub chunk_size: u32,
-}
-
-
 
 #[cfg(test)]
 mod tests {
@@ -512,3 +520,12 @@ mod tests {
     Ok(())
   }
 }
+
+
+
+/*
+  Need to refactor ChunkManger(Defer)
+  Make new features work first
+  Then refactor once approved
+*/
+
