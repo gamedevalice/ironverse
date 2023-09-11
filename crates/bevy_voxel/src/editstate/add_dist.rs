86c8e03dbbfbd9f2dd7c7855f1e6d3936d52048b
ChaoticGood <colete_nico@yahoo.com>
2023-09-11 15:26:09 +0800

    Integrated and simplified add voxel for cube and sphere

--- crates/bevy_voxel/src/editstate/add_dist.rs
+++ crates/bevy_voxel/src/editstate/add_dist.rs
@@ -2,6 +2,8 @@ use bevy::prelude::*;
 use voxels::data::voxel_octree::VoxelMode;
 use crate::{EditState, Preview, BevyVoxelResource, PreviewGraphics, Chunks, Center, ChunkData, ShapeState, MeshComponent};
 
+use super::{EditEvents, EditEvent};
+
 pub struct CustomPlugin;
 impl Plugin for CustomPlugin {
   fn build(&self, app: &mut App) {
@@ -14,69 +16,44 @@ impl Plugin for CustomPlugin {
 
 fn add_voxel_cube(
   mouse: Res<Input<MouseButton>>,
-  mut bevy_voxel_res: ResMut<BevyVoxelResource>,
-
-  mut chunks: Query<(&Preview, &Center, &mut Chunks, &mut MeshComponent)>,
+  mut chunks: Query<&Preview>,
   shape_state: Res<State<ShapeState>>,
+  
+  mut edit_event_writer: EventWriter<EditEvents>
 ) {
   if !mouse.just_pressed(MouseButton::Left) ||
   shape_state.0 != ShapeState::Cube {
     return;
   }
-  for (preview, center, mut chunks, mut mesh_comp) in &mut chunks {
+
+  for preview in &mut chunks {
     if preview.pos.is_none() {
       continue;
     }
-
-    let p = preview.pos.unwrap();
-    let res = bevy_voxel_res.set_voxel_cube(p, preview);
-
-    let mut all_chunks = Vec::new();
-    for (key, chunk) in res.iter() {
-      all_chunks.push(chunk.clone());
-      chunks.data.insert(*key, chunk.clone());
-    }
-
-    let data = bevy_voxel_res.load_mesh_data(&all_chunks);
-    for (mesh_data, handle) in data.iter() {
-      
-      mesh_comp.data.insert(mesh_data.key.clone(), mesh_data.clone());
-      mesh_comp.added.push((mesh_data.clone(), *handle));
-    }
+    edit_event_writer.send(EditEvents {
+      event: EditEvent::AddCube
+    });
   }
 }
 
 fn add_voxel_sphere(
   mouse: Res<Input<MouseButton>>,
-  mut bevy_voxel_res: ResMut<BevyVoxelResource>,
-
-  mut chunks: Query<(&Preview, &Center, &mut Chunks, &mut MeshComponent)>,
+  mut chunks: Query<&Preview>,
   shape_state: Res<State<ShapeState>>,
+  mut edit_event_writer: EventWriter<EditEvents>
 ) {
   if !mouse.just_pressed(MouseButton::Left) ||
   shape_state.0 != ShapeState::Sphere {
     return;
   }
 
-  for (preview, center, mut chunks, mut mesh_comp) in &mut chunks {
+  for preview in &mut chunks {
     if preview.pos.is_none() {
       continue;
     }
-
-    let p = preview.pos.unwrap();
-    let res = bevy_voxel_res.set_voxel_sphere(p, preview);
-
-    let mut all_chunks = Vec::new();
-    for (key, chunk) in res.iter() {
-      all_chunks.push(chunk.clone());
-      chunks.data.insert(*key, chunk.clone());
-    }
-
-    let data = bevy_voxel_res.load_mesh_data(&all_chunks);
-    for (mesh_data, handle) in data.iter() {
-      mesh_comp.data.insert(mesh_data.key.clone(), mesh_data.clone());
-      mesh_comp.added.push((mesh_data.clone(), *handle));
-    }
+    edit_event_writer.send(EditEvents {
+      event: EditEvent::AddSphere
+    });
   }
 }
 
--- crates/bevy_voxel/src/editstate/add_snap.rs
+++ crates/bevy_voxel/src/editstate/add_snap.rs
@@ -3,6 +3,8 @@ use utils::RayUtils;
 use voxels::data::voxel_octree::VoxelMode;
 use crate::{EditState, Preview, BevyVoxelResource, Center, Chunks, PreviewGraphics, ChunkData, ShapeState};
 
+use super::{EditEvents, EditEvent};
+
 
 pub struct CustomPlugin;
 impl Plugin for CustomPlugin {
@@ -99,67 +101,44 @@ fn set_distance(
 
 fn add_voxel_cube(
   mouse: Res<Input<MouseButton>>,
-  mut bevy_voxel_res: ResMut<BevyVoxelResource>,
-
-  mut chunks: Query<(&Preview, &Center, &mut Chunks)>,
+  mut chunks: Query<&Preview>,
   shape_state: Res<State<ShapeState>>,
+  
+  mut edit_event_writer: EventWriter<EditEvents>
 ) {
   if !mouse.just_pressed(MouseButton::Left) ||
   shape_state.0 != ShapeState::Cube {
     return;
   }
 
-  for (preview, center, mut chunks) in &mut chunks {
+  for preview in &mut chunks {
     if preview.pos.is_none() {
       continue;
     }
-
-    chunks.data.clear();
-    let p = preview.pos.unwrap();
-    bevy_voxel_res.set_voxel_cube(p, preview);
-
-    let all_chunks = bevy_voxel_res.load_adj_chunks_with_collider(center.key);
-    for chunk in all_chunks.iter() {
-      let data = bevy_voxel_res.compute_mesh(VoxelMode::SurfaceNets, chunk);
-      if data.positions.len() == 0 {
-        continue;
-      }
-      
-      chunks.data.insert(chunk.key, chunk.clone());
-    }
+    edit_event_writer.send(EditEvents {
+      event: EditEvent::AddCube
+    });
   }
 }
 
 fn add_voxel_sphere(
   mouse: Res<Input<MouseButton>>,
-  mut bevy_voxel_res: ResMut<BevyVoxelResource>,
-
-  mut chunks: Query<(&Preview, &Center, &mut Chunks)>,
+  mut chunks: Query<&Preview>,
   shape_state: Res<State<ShapeState>>,
+  mut edit_event_writer: EventWriter<EditEvents>
 ) {
   if !mouse.just_pressed(MouseButton::Left) ||
   shape_state.0 != ShapeState::Sphere {
     return;
   }
 
-  for (preview, center, mut chunks) in &mut chunks {
+  for preview in &mut chunks {
     if preview.pos.is_none() {
       continue;
     }
-
-    chunks.data.clear();
-    let p = preview.pos.unwrap();
-    bevy_voxel_res.set_voxel_sphere(p, preview);
-
-    let all_chunks = bevy_voxel_res.load_adj_chunks_with_collider(center.key);
-    for chunk in all_chunks.iter() {
-      let data = bevy_voxel_res.compute_mesh(VoxelMode::SurfaceNets, chunk);
-      if data.positions.len() == 0 {
-        continue;
-      }
-      
-      chunks.data.insert(chunk.key, chunk.clone());
-    }
+    edit_event_writer.send(EditEvents {
+      event: EditEvent::AddSphere
+    });
   }
 }
 
