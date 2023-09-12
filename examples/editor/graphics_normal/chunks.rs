f2d3e09b167e1d6f276d47c7a4c24fd38444b00b
ChaoticGood <colete_nico@yahoo.com>
2023-09-12 17:35:15 +0800

    Added the color back

--- examples/editor/graphics_normal/chunks.rs
+++ examples/editor/graphics_normal/chunks.rs
@@ -1,5 +1,5 @@
 use bevy::{prelude::*, render::{mesh::{MeshVertexAttribute, MeshVertexBufferLayout, Indices}, render_resource::{VertexFormat, AsBindGroup, ShaderRef, SpecializedMeshPipelineError, RenderPipelineDescriptor, PrimitiveTopology, ShaderType, AsBindGroupShaderType, TextureFormat}, render_asset::RenderAssets}, reflect::TypeUuid, pbr::{MaterialPipeline, MaterialPipelineKey, StandardMaterialFlags}, asset::LoadState};
-use bevy_voxel::{BevyVoxelResource, Chunks};
+use bevy_voxel::{BevyVoxelResource, Chunks, MeshComponent, Center};
 
 use crate::graphics::ChunkGraphics;
 
@@ -8,28 +8,34 @@ impl Plugin for CustomPlugin {
   fn build(&self, app: &mut App) {
     app
       .add_plugin(MaterialPlugin::<CustomMaterial>::default())
-      .add_system(add);
+      .add_system(add)
+      .add_system(remove);
   }
 }
 
 fn add(
-  bevy_voxel_res: Res<BevyVoxelResource>,
-
   mut commands: Commands,
   mut meshes: ResMut<Assets<Mesh>>,
   mut custom_materials: ResMut<Assets<CustomMaterial>>,
   mut images: ResMut<Assets<Image>>,
 
-  chunk_query: Query<(Entity, &Chunks), Changed<Chunks>>,
   chunk_graphics: Query<(Entity, &ChunkGraphics)>,
-) {
-  for (_, chunks) in &chunk_query {
-    for (entity, graphics) in &chunk_graphics {
-      commands.entity(entity).despawn_recursive();
-    }
 
-    for mesh in &chunks.data {
-      let data = &mesh.data;
+  mut chunk_query: Query<(Entity, &mut MeshComponent), Changed<MeshComponent>>,
+  mut bevy_voxel_res: ResMut<BevyVoxelResource>,
+) {
+  for (_, mut mesh_comp) in &mut chunk_query {
+    for (data, collider_handle) in mesh_comp.added.iter() {
+      'graphics: for (entity, graphics) in &chunk_graphics {
+        if graphics.key == data.key {
+          commands.entity(entity).despawn();
+
+          if graphics.lod == 0 {
+            bevy_voxel_res.physics.remove_collider(graphics.collider);
+          }
+          continue 'graphics;
+        }
+      }
 
       let mut render_mesh = Mesh::new(PrimitiveTopology::TriangleList);
       render_mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, data.positions.clone());
@@ -42,7 +48,7 @@ fn add(
         base_color: Color::rgb(1.0, 1.0, 1.0),
       });
 
-      let mut pos = bevy_voxel_res.get_pos(mesh.key);
+      let mut pos = bevy_voxel_res.get_pos(data.key);
       commands
         .spawn(MaterialMeshBundle {
           mesh: mesh_handle,
@@ -50,10 +56,76 @@ fn add(
           transform: Transform::from_translation(pos),
           ..default()
         })
-        .insert(ChunkGraphics);
+        .insert(ChunkGraphics { 
+          key: data.key, 
+          lod: data.lod as usize,
+          collider: *collider_handle,
+        });
+
+      // println!("data.lod {}", data.lod);
+    }
+    mesh_comp.added.clear();
+  }
+
+
+  // for (_, chunks) in &chunk_query {
+  //   for (entity, graphics) in &chunk_graphics {
+  //     commands.entity(entity).despawn_recursive();
+  //   }
+
+  //   for mesh in &chunks.data {
+
+  //     let mut render_mesh = Mesh::new(PrimitiveTopology::TriangleList);
+  //     render_mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, mesh.positions.clone());
+  //     render_mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, mesh.normals.clone());
+  //     render_mesh.set_indices(Some(Indices::U32(mesh.indices.clone())));
+  //     render_mesh.insert_attribute(VOXEL_COLOR, mesh.colors.clone());
+
+  //     let mesh_handle = meshes.add(render_mesh);
+  //     let material_handle = custom_materials.add(CustomMaterial {
+  //       base_color: Color::rgb(1.0, 1.0, 1.0),
+  //     });
+
+  //     let mut pos = bevy_voxel_res.get_pos(mesh.key);
+  //     commands
+  //       .spawn(MaterialMeshBundle {
+  //         mesh: mesh_handle,
+  //         material: material_handle,
+  //         transform: Transform::from_translation(pos),
+  //         ..default()
+  //       })
+  //       .insert(ChunkGraphics);
+  //   }
+  // }
+}
+
+fn remove(
+  mut commands: Commands,
+  mut meshes: ResMut<Assets<Mesh>>,
+  mut materials: ResMut<Assets<StandardMaterial>>,
+  chunk_graphics: Query<(Entity, &ChunkGraphics)>,
+
+  chunk_query: Query<(Entity, &Center)>,
+  mut bevy_voxel_res: ResMut<BevyVoxelResource>,
+) {
+
+  let ranges = bevy_voxel_res.ranges.clone();
+  for (_, center) in &chunk_query {
+    for (entity, graphics) in &chunk_graphics {
+
+      if !bevy_voxel_res.in_range_by_lod(&center.key, &graphics.key, graphics.lod) {
+        commands.entity(entity).despawn_recursive();
+
+        if graphics.lod == 0 {
+          bevy_voxel_res.physics.remove_collider(graphics.collider);
+          // println!("remove collider 2");
+        }
+      }
+      
     }
   }
 }
+
 
 pub const VOXEL_COLOR: MeshVertexAttribute =
   MeshVertexAttribute::new("VOXEL_COLOR", 988540918, VertexFormat::Float32x3);
