a8723fc935eb6fe928a9383f6128bf7402fb05da
gamedevalice-dev <124552025+gamedevalice-dev@users.noreply.github.com>
2023-10-04 09:29:33 -0400

    get multithreaded wasm to compile (#10)

--- crates/lib/multithread/index.js
+++ crates/lib/multithread/index.js
@@ -1,6 +1,6 @@
 export default async function (name) {
   // const pkgJs = await (await fetch(`./pkg/${name}.js`)).text();
-  const pkgJs = await (await fetch(`crates/multithread/pkg/${name}.js`)).text();
+  const pkgJs = await (await fetch(`crates/lib/multithread/pkg/${name}.js`)).text();
 
   // Create the 'pure' version of the wasm_bindgen's `init()`
   const initJs = `return () => { const document = undefined; const location = {}; ${pkgJs} return wasm_bindgen; };`;
@@ -8,7 +8,7 @@ export default async function (name) {
 
   const wbg = init();
   // const wasm = await wbg(`./pkg/${name}_bg.wasm`);
-  const wasm = await wbg(`crates/multithread/pkg/${name}_bg.wasm`);
+  const wasm = await wbg(`crates/lib/multithread/pkg/${name}_bg.wasm`);
   // console.log('wasm:', wasm);
   wbg.app();
 
